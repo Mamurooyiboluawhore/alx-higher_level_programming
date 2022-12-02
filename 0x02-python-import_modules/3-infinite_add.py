527c19baa6e6b0b9b351954c695f550be197a983
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2022-12-02 05:06:06 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,7 @@
-tast 3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    sum_var = 0
+    for i in range(1, len(argv)):
+        sum_var += int(argv[i])
+    print("{}".format(sum_var))
