b07bc07497584bec994c64c64dfd2dead42a5ae2
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2022-12-02 05:29:58 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-
+from sys import argv
 from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-if __name__ == "__main__":
-    a = 10
-    b = 5
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    result = add(int(argv[1]), int(argv[3]))
+elif argv[2] == '-':
+    result = sub(int(argv[1]), int(argv[3]))
+elif argv[2] == '*':
+    result = mul(int(argv[1]), int(argv[3]))
+elif argv[2] == '/':
+    result = div(int(argv[1]), int(argv[3]))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
 
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+print("{:s} {:s} {:s} = {:d}".format(argv[1], 
