12eebd8e93a01f1ecad75490031cb81d61e7e566
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-01-13 18:34:25 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,15 @@
-jsyk
+#!/usr/bin/python3
+"""
+    5-base_geometry: class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry
+        Attributes: None.
+        Methods:
+            area() - raises an Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
