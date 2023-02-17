835ebccc460c8da7c2843b721b76e2e3a76cbecb
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-02-17 02:06:54 +0100

    Update 13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
--- this is acript that removes all records with with a >=5 in the table second_table in the databse hbtn_0c_0
-
-DELETE FROM second_table WHERE score >=5;
+-- removes all records where score is <= 5 from
+-- second table
+DELETE FROM second_table WHERE score <= 5;
