ad400966ede46fb21d0b27c30489c6a22ffb9e48
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-02-17 02:07:53 +0100

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,6 @@
--- a script that list all records with a scoree >=10 in the second_table in the database hbtn_0c_0
-
-SELECT score, name FROM second_table WHERE >= 10 ORDER BY score DESC;
+-- lists all rows of the table second_table
+-- from the database hbtn_0c_0
+-- results display score and name
+-- ordered by score
+-- with score >= 10
+SELECT score, name FROM second_table HAVING score >= 10 ORDER BY score DESC;
