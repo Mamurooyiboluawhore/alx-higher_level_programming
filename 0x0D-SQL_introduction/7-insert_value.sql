7a313092736c10bd4d56b6f64556e15593489075
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-02-17 02:17:12 +0100

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- this is a script that input a new row in the table first_table in the hbtn_0c_0 databse in MySQL server
-
-INSERT INFO `first_table` (`id`, `name`) VALUES(89, 'Best School');
+-- inserts a new row in table first_table
+-- in database hbtn_0c_0
+INSERT INTO first_table(id, name) VALUES (89, 'Best School');
