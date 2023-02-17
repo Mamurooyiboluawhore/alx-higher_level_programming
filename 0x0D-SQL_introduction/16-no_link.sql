0266e8bd78026f3f5d6d4264478f4ff014bebb35
Mamurooyiboluawhore <111292261+Mamurooyiboluawhore@users.noreply.github.com>
2023-02-17 01:30:57 +0100

    Create 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- a script that list all records of the table second_table in the database hbtn_0c_0 in the mysql server
+
+SELECT score, name FROM second_table WWHERE is IS NOT NULL AND name != '' ORDER BY score DESC;
