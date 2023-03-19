5e2e60f073d89f48c94bbe91058d921cf8eb3dd0
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2023-03-19 00:58:47 +0000

    js_file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let element = 0; element < myArray.length; element++) {
+  console.log(myArray[element]);
+}
