67c909b90c87c00e3840bc2a151a2505323a8571
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-04-26 00:26:28 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a script that writes an empty class called square"""
+
+
+class Square:
+    """ a class with a private instance attribute"""
+    def __init__(self, size):
+        self.__size = size
